dd661c5370761fd3e1670b7a9dada651f4e319a1
Glen Haber <glen.haber@thebridgecorp.com>
2021-12-12 14:16:59 -0500

    Improve day 12
    Track backtracking using a flag instead of always iterating over the vec

--- src/day12.rs
+++ src/day12.rs
@@ -1,4 +1,5 @@
 use std::collections::{HashMap, HashSet, VecDeque};
+use std::vec;
 
 enum Size {
     Big,
@@ -47,43 +48,33 @@ pub fn part1(input: String) {
 }
 
 pub fn part2(input: String) {
-    let mut map: HashMap<&str, HashSet<&str>> = HashMap::new();
+    let mut map: HashMap<&str, Vec<&str>> = HashMap::new();
 
     for line in input.lines() {
         let (a, b) = line.split_once("-").unwrap();
         if a != "end" && b != "start" {
-            map.entry(a).or_insert(HashSet::new()).insert(b);
+            map.entry(a).or_insert(Vec::new()).push(b);
         }
         if a != "start" && b != "end" {
-            map.entry(b).or_insert(HashSet::new()).insert(a);
+            map.entry(b).or_insert(Vec::new()).push(a);
         }
     }
 
-    // let mut complete_paths: HashSet<Vec<&str>> = HashSet::new();
     let mut total_paths = 0;
-    let mut paths: VecDeque<Vec<&str>> = VecDeque::from(vec![vec!["start"]]);
+    let mut paths: VecDeque<(Vec<&str>, bool)> = VecDeque::from(vec![(vec!["start"], false)]);
     while !paths.is_empty() {
-        let path = paths.pop_front().unwrap();
-        // Check for previous revisit
-        let mut revisited = false;
-        let mut seen = HashSet::new();
-        for &cave in path.iter() {
-            if seen.contains(cave) && matches!(size(cave), Small) {
-                revisited = true;
-                break;
+        let (path, has_backtrack) = paths.pop_front().unwrap();
+        for &next in map.get(path.last().unwrap()).unwrap() {
+            let mut backtrack = has_backtrack;
+            if matches!(size(next), Small) && path.contains(&next) {
+                if backtrack { continue; } else { backtrack = true; }
             }
-            seen.insert(cave);
-        }
-        for &next in map.get(path.clone().last().unwrap()).unwrap() {
-            if matches!(size(next), Small) && path.contains(&next) && revisited {
-                continue;
-            }
-            let mut new_path = path.clone();
-            new_path.push(next);
             if next == "end" {
                 total_paths += 1;
             } else {
-                paths.push_back(new_path);
+                let mut new_path = path.clone();
+                new_path.push(next);
+                paths.push_back((new_path, backtrack));
             }
         }
     }

1ca5703d724cab5c1f9067fc29558b857c918ed4
Glen Haber <glen.haber@thebridgecorp.com>
2021-12-15 11:22:16 -0500

    Day 15 (part 2)

--- src/day15.rs
+++ src/day15.rs
@@ -1,84 +1,65 @@
 use std::collections::{HashMap, VecDeque};
 
-type Coord = (i32, i32);
+type Coord = (usize, usize);
 
-const UP: Coord = (-1, 0);
-const DOWN: Coord = (1, 0);
-const LEFT: Coord = (0, -1);
-const RIGHT: Coord = (0, 1);
-const DIRECTIONS: [Coord; 4] = [UP, DOWN, LEFT, RIGHT];
+fn adjusted_risk(risk: usize, offset: usize) -> usize {
+    let mut new_risk = risk + offset;
+    while new_risk > 9 {
+        new_risk -= 9;
+    }
+    new_risk
+}
 
-fn solve_old(input: String, tiles: i32) -> usize {
-    let grid: Vec<Vec<usize>> = input.lines().map(|line| {
-        line.chars().map(|ch| {
-            ch.to_string().parse::<usize>().unwrap()
-        }).collect::<Vec<usize>>()
-    }).collect();
-    let width: i32 = grid.iter().next().unwrap().len() as i32;
-    let height: i32 = grid.len() as i32;
-
+fn solve(input: String, tiles: usize) -> usize {
+    let tile_width = input.lines().next().unwrap().len();
+    let tile_height = input.lines().count();
     let mut risk_map: HashMap<Coord, usize> = HashMap::new();
-    risk_map.insert((0, 0), 0);
-    let mut queue: VecDeque<(Coord, usize)> = VecDeque::new();
-    queue.push_back(((0, 0), 0));
-    while !queue.is_empty() {
-        let ((x, y), risk) = queue.pop_front().unwrap();
-        for (dx, dy) in DIRECTIONS {
-            let new_x = x + dx;
-            let new_y = y + dy;
-            if new_x < 0 || new_x > (width * tiles) - 1 || new_y < 0 || new_y > (height * tiles) - 1 { continue; }
-            let current_risk = match risk_map.get(&(new_x, new_y)) {
-                Some(n) => *n,
+    for (r, row) in input.lines().enumerate() {
+        for (c, risk) in row.chars().enumerate() {
+            let risk = risk.to_string().parse().unwrap();
+            for h_tile in 0usize..tiles {
+                for v_tile in 0usize..tiles {
+                    let coord = (r + 1 + v_tile * tile_height, c + 1 + h_tile * tile_width);
+                    let new_risk = adjusted_risk(risk, h_tile + v_tile);
+                    risk_map.insert(coord, new_risk);
+                }
+            }
+        }
+    }
+
+    let get_neighbors = |(r, c): (usize, usize)| {
+        let mut neighbors = vec![];
+        for (dr, dc) in [(0, 1), (0, -1), (1, 0), (-1, 0)] {
+            let current_r = (r as i32) + dr;
+            let current_c = (c as i32) + dc;
+            if current_r < 0 || current_c < 0 { continue; }
+            let key = (current_r as usize, current_c as usize);
+            if risk_map.contains_key(&key) {
+                neighbors.push(key);
+            }
+        }
+        neighbors
+    };
+
+    let mut risks: HashMap<Coord, usize> = HashMap::from([((1, 1), 0)]);
+    let mut spelunking: VecDeque<(Coord, usize)> = VecDeque::from([((1, 1), 0)]);
+
+    while !spelunking.is_empty() {
+        let (pos, path_risk) = spelunking.pop_front().unwrap();
+        for neighbor in get_neighbors(pos) {
+            let current_risk = match risks.get(&neighbor) {
+                Some(&r) => r,
                 None => usize::MAX,
             };
-            let mut added_risk = grid[(new_x % width) as usize][(new_y % height) as usize] + (new_x / 10 + new_y / 10) as usize;
-            while added_risk > 9 { added_risk -= 9 };
-            let new_risk = risk + added_risk;
+            let new_risk = path_risk + risk_map[&neighbor];
             if new_risk < current_risk {
-                risk_map.insert((new_x, new_y), new_risk);
-                queue.push_back(((new_x, new_y), new_risk));
+                risks.insert(neighbor, new_risk);
+                spelunking.push_back((neighbor, new_risk));
             }
         }
     }
-    // dbg!(&risk_map);
-    risk_map[&((width * tiles) - 1, (height * tiles) - 1)]
-}
-
-struct Grid {
-    base: Vec<Vec<usize>>,
-    num_tiles: usize,
-}
-
-impl Grid {
-    fn get_risk(&self, x: usize, y: usize) -> Option<usize> {
-        // println!("Requested {},{}",x,y);
-        let tile_width = self.base.iter().next().unwrap().len();
-        let tile_height = self.base.len();
-        if x > tile_width * self.num_tiles - 1 || y > tile_height * self.num_tiles - 1 {
-            None
-        } else {
-            let mut add = 0;
-            let mut base_x = x;
-            let mut base_y = y;
-            while base_x >= tile_width {
-                base_x -= tile_width;
-                add += 1;
-            }
-            while base_y >= tile_height {
-                base_y -= tile_height;
-                add += 1;
-            }
-            // println!("{},{} => {},{}", x, y, base_x, base_y);
-            let mut risk = self.base[base_y][base_x];
-            for _ in 0..add {
-                risk = match risk {
-                    9 => 1,
-                    _ => risk + 1
-                };
-            }
-            Some(risk)
-        }
-    }
+    let target = risk_map.keys().max().unwrap();
+    *risks.get(target).unwrap()
 }
 
 pub fn part1(input: String) {
@@ -92,8 +73,8 @@ pub fn part1(input: String) {
 3125421639
 1293138521
 2311944581".to_string();
-    assert_eq!(solve_old(sample.clone(), 1), 40);
-    println!("{}", solve_old(input, 1));
+    assert_eq!(solve(sample.clone(), 1), 40);
+    println!("{}", solve(input, 1));
 }
 
 pub fn part2(input: String) {
@@ -107,6 +88,6 @@ pub fn part2(input: String) {
 3125421639
 1293138521
 2311944581".to_string();
-    assert_eq!(solve_old(sample.clone(), 5), 315);
-    println!("{}", solve_old(input, 5));
+    assert_eq!(solve(sample.clone(), 5), 315);
+    println!("{}", solve(input, 5));
 }
